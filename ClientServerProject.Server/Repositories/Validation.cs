f62498fce1ac2c57ee9dd29741da2c677a8920b2
AleksManolow <amanolov7@gmail.com>
2024-04-10 17:49:21 +0300

    Create class Validation

---
+++ ClientServerProject.Server/Repositories/Validation.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ClientServerProject.Server.Repositories
+{
+    public class Validation
+    {
+        public bool ValidateEmail(string email)
+        {
+            bool isEmail = Regex.IsMatch(email, 
+                @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", 
+                RegexOptions.IgnoreCase);
+
+            return isEmail;
+        }
+
+        public bool ValidateName(string name)
+        {
+            
+            return !string.IsNullOrEmpty(name);
+        }
+
+        public bool ValidatePassword(string password)
+        {
+            
+            return password.Length >= 6; 
+        }
+    }
+}
