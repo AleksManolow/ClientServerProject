03053b48dc53ef69b6e94115fba0069b3c925efe
AleksManolow <amanolov7@gmail.com>
2024-04-10 14:39:30 +0300

    Create User model

---
+++ ClientServerProject.Server/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace ClientServerProject.Server.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public bool IsVerified { get; set; }
+    }
+}
